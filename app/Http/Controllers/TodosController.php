0e0f191d7a4367ec056b9ec12772c50ab8fa46a8
Daniël van Drunen <daniel@pixely.me>
2017-09-21 15:59:10 +0200

    Add TodosController

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodosController extends Controller {
+    public function index() {
+        return 'Hello world';
+    }
+}
