704a07fb3658b97091332f162495b18d4612b8ac
Daniël van Drunen <daniel@pixely.me>
2017-09-21 20:00:36 +0200

    Get rid of default heading in App component

--- resources/assets/js/components/App.js
+++ resources/assets/js/components/App.js
@@ -6,7 +6,6 @@ export default class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello from React</h1>
                 <Todos />
             </div>
         )
