2e679c60577456fa6af10ceb7240a12d9621c5e7
Daniël van Drunen <daniel@pixely.me>
2017-09-21 20:00:20 +0200

    Move fetchTodos function from App to Todos component

--- resources/assets/js/components/App.js
+++ resources/assets/js/components/App.js
@@ -1,37 +1,13 @@
 import React from 'react'
-import axios from 'axios'
 
 import Todos from './Todos'
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            todos: []
-        }
-    }
-
-    componentDidMount() {
-        this.fetchTodos()
-    }
-
-    fetchTodos() {
-        axios.get('/api/todos')
-            .then(response => {
-                const json = response.data
-
-                this.setState({
-                    todos: json
-                })
-            })
-    }
-
     render() {
         return (
             <div>
                 <h1>Hello from React</h1>
-                <Todos todos={this.state.todos} />
+                <Todos />
             </div>
         )
     }
--- resources/assets/js/components/Todos.js
+++ resources/assets/js/components/Todos.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import axios from 'axios'
 
 import Todo from './Todo'
 
 export default class Todos extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            todos: []
+        }
+    }
+
+    componentDidMount() {
+        this.fetchTodos()
+    }
+
+    fetchTodos() {
+        axios.get('/api/todos')
+            .then(response => {
+                const json = response.data
+
+                this.setState({
+                    todos: json
+                })
+            })
+    }
+
     render() {
-        const todos = this.props.todos.map(todo => {
+        const todos = this.state.todos.map(todo => {
             return <Todo description={todo.description} completed={todo.completed} />
         })
 
