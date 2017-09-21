d62f2ff1a8797746c046c86553de9cb711e7f07b
Daniël van Drunen <daniel@pixely.me>
2017-09-21 21:17:14 +0200

    Add input for new todo to App component

--- resources/assets/js/components/App.js
+++ resources/assets/js/components/App.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import axios from 'axios'
 
 import Todos from './Todos'
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            todo: ''
+        }
+    }
+
+    handleChange(e) {
+        this.setState({
+            todo: e.target.value
+        })
+    }
+
+    handleKeyDown(e) {
+        if (e.keyCode === 13) {
+            axios.post('/api/todos', {
+                description: this.state.todo
+            })
+
+            this.setState({
+                todo: ''
+            })
+        }
+    }
+
     render() {
         return (
             <div>
+                <input
+                    onChange={this.handleChange.bind(this)}
+                    onKeyDown={this.handleKeyDown.bind(this)}
+                />
                 <Todos />
             </div>
         )
