002e60a7fea4971db0861abb008f65b97c218f35
Daniël van Drunen <daniel@pixely.me>
2017-09-21 16:03:51 +0200

    Add model/migration for todos

---
+++ database/migrations/2017_09_21_140142_create_todos_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodosTable extends Migration {
+    public function up() {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description');
+            $table->boolean('completed');
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('todos');
+    }
+}
