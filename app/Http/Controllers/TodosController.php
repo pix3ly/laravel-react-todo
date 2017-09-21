4786c7b0c6b79f0dbb6e2fff0761f5ed2e9d7b04
Daniël van Drunen <daniel@pixely.me>
2017-09-21 16:07:35 +0200

    Add index/show functions to TodosController

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Todo;
+
 class TodosController extends Controller {
     public function index() {
-        return 'Hello world';
+        return Todo::all();
+    }
+
+    public function show($id) {
+        return Todo::find($id);
     }
 }
