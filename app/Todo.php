0e31cbff845947d228e3d3b7dcb69a68db92d516
Daniël van Drunen <daniel@pixely.me>
2017-09-21 17:36:36 +0200

    Turn off timestamps for Todo model and add fillable fields

--- app/Todo.php
+++ app/Todo.php
@@ -5,5 +5,9 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model {
-    //
+    public $timestamps = false;
+
+    protected $fillable = [
+        'description', 'completed'
+    ];
 }
